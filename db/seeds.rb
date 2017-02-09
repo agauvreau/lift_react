697e30250476d3beea298434b77cd368dd7b783a
Adam Gauvreau <adamgauvreau@hotmail.com>
2017-02-09 14:54:33 +0000

    Added React lift table to index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Lift.create(date: Date.today, liftname: "Lift1", ismetric: 1, weightlifted: 200, repsperformed: 10, onerm: 250)
+Lift.create(date: Date.today, liftname: "Lift2", ismetric: 1, weightlifted: 190, repsperformed: 15, onerm: 240)
+Lift.create(date: Date.today, liftname: "Lift3", ismetric: 1, weightlifted: 180, repsperformed: 20, onerm: 230)
+Lift.create(date: Date.today, liftname: "Lift4", ismetric: 1, weightlifted: 170, repsperformed: 25, onerm: 220)
